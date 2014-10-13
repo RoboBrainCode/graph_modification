958213e153c05baf412ddc6244645ade4b74ffe1
Arzav Jain <arzavj@stanford.edu>
2014-10-12 17:42:33 -0700

    fixing conflicts

--- build_graph.py
+++ build_graph.py
@@ -13,14 +13,11 @@ def main(args):
     db = client[DBNAME]
     json_feeds = db['json_feeds']
     all_feeds = json_feeds.find()
-    count = 0
+
     for feed in all_feeds:
         feed['_id'] = feed['_id'].__str__()
         feed['created_at'] = feed['created_at'].isoformat()
-        print feed
         add_feed_to_graph(feed)
-        count += 1
-        if count > 1: break
 
 if __name__ == "__main__":
     main(sys.argv)
