e84dfd1cb68cc18322d58f35b110053e4940fd05
Arzav Jain <arzavj@stanford.edu>
2014-11-20 15:10:56 -0800

    added pid stuff

--- add_to_graph_daemon.py
+++ add_to_graph_daemon.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+import os
+import sys
 import boto
 import json
 import traceback
@@ -19,31 +21,49 @@ feed_queue.set_message_class(RawMessage)
 NUM_SECONDS_TO_POLL = 20
 MSG_BATCH_SIZE = 10
 
-def main():
-    while True:
-        queries = []
-        feeds_to_update = []
-        messages = feed_queue.get_messages(
-            num_messages=MSG_BATCH_SIZE, wait_time_seconds=NUM_SECONDS_TO_POLL)
-        print "Messages: ", messages
-        for m in messages:
-            json_feed = json.loads(m.get_body(), object_hook=json_util.object_hook)
-            if feed_has_already_been_added(json_feed): continue
-            temp_queries = []
-            try:
-                append_cypher_queries(json_feed, temp_queries)
-            except Exception, e:
-                print traceback.format_exc()
-                print json_feed
-            else:
-                for q in temp_queries:
-                    queries.append(q)
-                feeds_to_update.append(json_feed)
+PIDFILE = "/tmp/add_to_graph_daemon.pid"
 
-        if len(queries) > 0:
-            submit_batch(queries, feeds_to_update)
-        if len(messages) > 0:
-            feed_queue.delete_message_batch(messages)
+def write_pid_to_file():
+    pid = str(os.getpid())
+    if os.path.isfile(PIDFILE):
+        print "%s already exists, exiting." % PIDFILE
+        sys.exit()
+    else:
+        file(PIDFILE, 'w').write(pid)
+
+def main():
+    write_pid_to_file()
+    try:
+
+        while True:
+            queries = []
+            feeds_to_update = []
+            messages = feed_queue.get_messages(num_messages=MSG_BATCH_SIZE, 
+                wait_time_seconds=NUM_SECONDS_TO_POLL)
+            for m in messages:
+                json_feed = json.loads(
+                    m.get_body(), object_hook=json_util.object_hook)
+                if feed_has_already_been_added(json_feed):
+                    print 'feed has already been added'
+                    continue
+                temp_queries = []
+                try:
+                    append_cypher_queries(json_feed, temp_queries)
+                except Exception, e:
+                    print traceback.format_exc()
+                    print json_feed
+                else:
+                    for q in temp_queries:
+                        queries.append(q)
+                    feeds_to_update.append(json_feed)
+
+            if len(queries) > 0:
+                submit_batch(queries, feeds_to_update)
+            if len(messages) > 0:
+                feed_queue.delete_message_batch(messages)
+
+    finally:
+        os.unlink(PIDFILE)
 
 if __name__ == '__main__':
     main()
