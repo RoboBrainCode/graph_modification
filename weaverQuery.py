22cfadb6157bb0335667ba3ec8ffb078011699d4
Ubuntu <ubuntu@ip-172-31-33-213.us-west-2.compute.internal>
2015-06-09 19:39:12 +0000

    minor

--- weaverQuery.py
+++ weaverQuery.py
@@ -20,15 +20,15 @@ def getUpdatedProperty(newProps,oldProps):
 	print 'update property'
 	print oldProps
 	print newProps
-	for key,val in nodeProps.iteritems():
+	for key,val in newProps.iteritems():
 			updateVal=val
 			if key in oldProps:
-				updateVal=mergeProps(nodeProps[key],oldProps[key])
-			nodeProps[key]=updateVal
+				updateVal=mergeProps(newProps[key],oldProps[key])
+			newProps[key]=updateVal
 
-	print nodeProps
+	print newProps
 	print 'end of update property'
-	return nodeProps
+	return newProps
 
 
 def InsertConcept(Id='floor',CreatedAt=datetime.datetime.now(),nodeProps={}):
@@ -45,11 +45,12 @@ def InsertConcept(Id='floor',CreatedAt=datetime.datetime.now(),nodeProps={}):
 	    node=c.get_node(node=Id)
 	except client.WeaverError:
 		print 'node already exists'
+		nodeProps.pop("created_at", None)
 		node=c.get_node(node=Id)
 		print node.properties
 		oldProps=c.get_node(node=Id).properties
 		c.begin_tx()
-		c.set_node_properties(node=handle,properties=getUpdatedProperty(nodeProps,oldProps))
+		c.set_node_properties(node=Id,properties=getUpdatedProperty(nodeProps,oldProps))
 		try:
 			c.end_tx()
 		except client.WeaverError:
@@ -131,7 +132,7 @@ def InsertNewRelation(CreatedAt=datetime.datetime.now(),src='1',dst='2',edgeProp
 	InsertRelation(CreatedAt=CreatedAt,src=src,dst=dst,edgeDirection='B',edgeProps=edgeProps)
 
 if __name__ == "__main__":
-	InsertConcept(Id='floor',CreatedAt=datetime.datetime.now(),nodeProps={'pk': 'prop1', 'feed_id': '5576848d76b9a67abccd8073', 'handle': 'floor', 'prop2': '123', 'label': 'Concept'})
+	InsertConcept(Id='floor',CreatedAt=datetime.datetime.now(),nodeProps={'pk': 'prop1,prop2', 'feed_id': '5576848d76b9a67abccd8073,modified', 'handle': 'floor123', 'prop2': '123', 'label': 'Concept,Image'})
 	read_props = c.get_node_properties('floor')
 	print read_props
 	InsertConcept(Id='wall',CreatedAt=datetime.datetime.now(),nodeProps={'pk': 'prop1', 'feed_id': '5576848d76b9a67abccd8073', 'handle': 'wall', 'prop2': '123', 'label': 'Concept'})
